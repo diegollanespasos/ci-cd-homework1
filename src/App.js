bcfc2b70d24f5f9f876904145cbbe29c1a6bd8f3
diegollanespasos <diego.llanes@theksquaregroup.com>
2021-03-28 19:15:19 -0600

    added a change with a h2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello, my name is Diego Llanes</h1>
+        <h2>Im making new changes</h2>
       </header>
     </div>
   );
